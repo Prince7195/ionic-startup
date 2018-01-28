beaf44b60251bf804e5990793572ca218befeb40
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-01-28 19:10:27 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(private navCtrl: NavController, private navParams: NavParams) {}
 
-  constructor(public navCtrl: NavController) {
-
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad HomePage");
   }
 
+  navigateToAboutPage(): void {
+    this.navCtrl.push("AboutPage", {
+      message: "Hello from Home page"
+    });
+  }
 }
